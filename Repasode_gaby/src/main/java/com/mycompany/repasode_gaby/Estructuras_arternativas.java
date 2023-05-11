9adc6aa371f6ccbf68ab99aa6a04d11751496dbb
GABRIELA <91220649+Gabrielaarecisneros@users.noreply.github.com>
2023-05-11 20:05:56 +0200

    Add files via upload

---
+++ Repasode_gaby/src/main/java/com/mycompany/repasode_gaby/Estructuras_arternativas.java
@@ -0,0 +1,31 @@
+////Veamos un ejemplo sencillo. Un programa que lee por teclado el precio inicial de un producto, si es
+////superior a 200 € se le aplica un descuento de 30 €, y por último lo muestra por pantalla.
+//package com.mycompany.repasode_gaby;
+//
+///**
+// *
+// * @author DAW
+// */
+//public class Estructuras_arternativas {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        int;
+//        
+//        System.out.println("precio>200");{}
+//        { else
+//        for(int"precio=precio-30" precio<200);
+//        if(200)
+//        
+//        Scanner sc=new Scanner(System.in);
+//        
+//        
+//    }
+//        
+//                
+//      
+//    }
+//    
+//
